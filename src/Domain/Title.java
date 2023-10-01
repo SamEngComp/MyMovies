dd31bba7cd7da1d1ebf52fac9c64a17316e781ae
Samuel S. Furtado <salessamuel268@gmail.com>
2023-10-01 15:21:38 -0300

    feat: add Title class

---
+++ src/Domain/Title.java
@@ -0,0 +1,71 @@
+package Domain;
+
+public class Title {
+
+    // PROPERTIES
+
+    private String name;
+    private int releaseYear;
+    private boolean includedInPlan;
+    private double sumRatings;
+    private int totalRatings;
+    private int durationInMinutes;
+
+    // METHODS
+
+    void toAssess(double note) {
+        sumRatings += note;
+        totalRatings ++;
+    }
+
+    void showTechnicalSheet() {
+        System.out.println("Movie name: " + this.name);
+        System.out.println("Release year: " + this.releaseYear);
+    }
+
+    // GET AND SET
+
+    public double getAverage() {
+        return sumRatings / totalRatings;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public boolean isIncludedInPlan() {
+        return includedInPlan;
+    }
+
+    public void setIncludedInPlan(boolean includedInPlan) {
+        this.includedInPlan = includedInPlan;
+    }
+
+    public double getSumRatings() {
+        return sumRatings;
+    }
+
+    public int getTotalRatings() {
+        return totalRatings;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+}
