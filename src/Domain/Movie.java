4b40587ec6bf2c9ae57fbf75f80ad282c22088e4
Samuel S. Furtado <salessamuel268@gmail.com>
2023-10-01 15:31:09 -0300

    feat: add Movie class

---
+++ src/Domain/Movie.java
@@ -0,0 +1,22 @@
+package Domain;
+
+public class Movie extends Title {
+
+    // PROPERTIES
+
+    public String getDirector() {
+        return director;
+    }
+
+    // GET AND SET
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    private String director;
+
+
+
+
+}
